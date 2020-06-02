77d7ffc781e6ebabdce93080a04bc763e3cdbd3e
MaksymKmets <maksym.kmets@gmail.com>
2020-06-02 16:24:11 +0300

    Adding OffsetDateTimeSupport

--- core/src/main/java/com/flax/finplat/config/WebConfig.java
+++ core/src/main/java/com/flax/finplat/config/WebConfig.java
@@ -4,6 +4,7 @@ import com.fasterxml.jackson.core.JsonGenerator;
 import com.fasterxml.jackson.core.JsonParser;
 import com.fasterxml.jackson.databind.*;
 import com.fasterxml.jackson.databind.module.SimpleModule;
+import com.fasterxml.jackson.datatype.jsr310.JavaTimeModule;
 import com.flax.finplat.common.Calculator;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.converter.HttpMessageConverter;
@@ -15,12 +16,15 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 import java.io.IOException;
 import java.math.BigDecimal;
+import java.time.OffsetDateTime;
+import java.time.format.DateTimeFormatter;
 import java.util.List;
 import java.util.Optional;
 
 @Configuration
 @EnableWebMvc
 public class WebConfig implements WebMvcConfigurer {
+
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**")
@@ -37,19 +41,30 @@ public class WebConfig implements WebMvcConfigurer {
      */
     @Override
     public void extendMessageConverters(List<HttpMessageConverter<?>> converters) {
-        Optional<HttpMessageConverter<?>> first = converters.stream().filter(
+        Optional<HttpMessageConverter<?>> messageConverter = converters.stream().filter(
                 c -> c instanceof MappingJackson2HttpMessageConverter).findFirst();
-        if (first.isPresent()) {
-            final AbstractJackson2HttpMessageConverter converter = (AbstractJackson2HttpMessageConverter) first.get();
-            converter.getObjectMapper().enable(SerializationFeature.INDENT_OUTPUT);
-            converter.getObjectMapper().enable(DeserializationFeature.FAIL_ON_UNKNOWN_PROPERTIES);
+        messageConverter.ifPresent(httpMessageConverter -> {
+            final AbstractJackson2HttpMessageConverter converter = (AbstractJackson2HttpMessageConverter) httpMessageConverter;
             converter.getObjectMapper()
+                    .enable(SerializationFeature.INDENT_OUTPUT)
+                    .enable(DeserializationFeature.FAIL_ON_UNKNOWN_PROPERTIES)
                     .registerModule(new SimpleModule()
                                             .addSerializer(BigDecimal.class, new MoneySerializer())
-                                            .addDeserializer(BigDecimal.class, new MoneyDeserializer()));
-        }
+                                            .addDeserializer(BigDecimal.class, new MoneyDeserializer())
+                                            .addSerializer(OffsetDateTime.class, new OffsetDateTimeSerializer()))
+                    .registerModule(new JavaTimeModule());
+        });
     }
 
+    private static class OffsetDateTimeSerializer extends JsonSerializer<OffsetDateTime> {
+        @Override
+        public void serialize(OffsetDateTime offsetDateTime, JsonGenerator jsonGenerator,
+                              SerializerProvider serializerProvider) throws IOException {
+            jsonGenerator.writeString(
+                    DateTimeFormatter.ISO_LOCAL_DATE_TIME.format(
+                            offsetDateTime));
+        }
+    }
 
     private static class MoneySerializer extends JsonSerializer<BigDecimal> {
         @Override
